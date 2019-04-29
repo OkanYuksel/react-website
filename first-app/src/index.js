cfa4b7a01d47676b6f3a50e2ff8cd91c2519c9c2
OkanYuksel <okannnyuksel@gmail.com>
2019-04-30 00:23:28 +0300

    Update index.js
    Code formatting

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -7,29 +7,29 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import {Root} from "./Root";
-import {Home} from "./Home";
-import {User} from "./User";
-import {About} from "./About";
-import {Contact} from "./Contact";
+import { Root } from "./Root";
+import { Home } from "./Home";
+import { User } from "./User";
+import { About } from "./About";
+import { Contact } from "./Contact";
 
 var user = {
   id: "12345678",
-  userName: "Admin User", 
+  userName: "Admin User",
   friends: [
     {
       userName: "User Profile 1",
       userURL: "/Books"
-    },{
+    }, {
       userName: "User Profile 2",
       userURL: "#2"
-    },{
+    }, {
       userName: "User Profile 3",
       userURL: "#3"
-    },{
+    }, {
       userName: "User Profile 4",
       userURL: "#4"
-    },{
+    }, {
       userName: "User Profile 5",
       userURL: "#5"
     }
@@ -37,64 +37,55 @@ var user = {
   onlineStatus: true
 };
 
-class UserProfile extends React.Component{
+class UserProfile extends React.Component {
 
-
-
-	render(){
-		let userOnlineStatusCssClassName = "online_status";
-		if (this.props.userData.onlineStatus){
-			userOnlineStatusCssClassName += " show";
-		}
-		const friendList = this.props.userData.friends.map(function(friend, index) {
-			return < FriendItem friendProperty={friend} key={index}/>
-		});
-		return(
-			<div className="user_profile">
-				<span className="user_name">{this.props. userData.userName}</span>
+  render() {
+    let userOnlineStatusCssClassName = "online_status";
+    if (this.props.userData.onlineStatus) {
+      userOnlineStatusCssClassName += " show";
+    }
+    const friendList = this.props.userData.friends.map(function (friend, index) {
+      return < FriendItem friendProperty={friend} key={index} />
+    });
+    return (
+      <div className="user_profile">
+        <span className="user_name">{this.props.userData.userName}</span>
         &nbsp;
-				<span className={userOnlineStatusCssClassName} style={{color:'blue'}}>Online</span>
-				<ul className="user_friends">{friendList}</ul>
-        
-
+				<span className={userOnlineStatusCssClassName} style={{ color: 'blue' }}>Online</span>
+        <ul className="user_friends">{friendList}</ul>
 
         <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+            <hr />
 
-        <hr />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+          </div>
+        </Router>
       </div>
-    </Router>
-
-			</div>
-
-        ); 
-    }
+    );
+  }
 }
 
-
-
-function FriendItem (props) {
-	return(
-		<li>
-			<a>{'Username: ' + props.friendProperty.userName + ' URL: ' +  props.friendProperty.userURL }</a>
-		</li>
-	);
+function FriendItem(props) {
+  return (
+    <li>
+      <a>{'Username: ' + props.friendProperty.userName + ' URL: ' + props.friendProperty.userURL}</a>
+    </li>
+  );
 }
 
-ReactDOM.render(<UserProfile userData={user}/>, document.getElementById("root"));
+ReactDOM.render(<UserProfile userData={user} />, document.getElementById("root"));
 serviceWorker.unregister();
